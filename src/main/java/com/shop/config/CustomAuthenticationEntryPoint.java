8764386097056b83747e7421bef161eaa00564ce
heesancreat <heesan2572@gamil.com>
2023-10-07 12:20:06 +0900

    first upload

---
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.shop.config;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+        if ("XMLHttpRequest".equals(request.getHeader("X-requested-with"))){
+
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        }
+        else{
+            response.sendRedirect("/members/login");
+        }
+    }
+}
